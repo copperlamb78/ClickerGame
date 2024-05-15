38fd03c4e7bd2b29eb2a6732611eefb45e1af00a
copperlamb78 <nascimentogabriel.2004@gmail.com>
2024-05-15 00:16:34 -0300

    add Rebirth function

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -6,8 +6,10 @@ import styles from './Game.module.css'
 
 export function Game() {
     
+    const [rebirth, setRebirth] = useState(1)
+    const [rebirthPrice, setRebirthPrice] = useState(500000)
 
-    const [upgradeClickAdd, setUpgradeClickAdd] = useState(1)
+    const [upgradeClickAdd, setUpgradeClickAdd] = useState(1000)
     const [clickPerSecondAmount, setClickPerSecondAmount] = useState(0)
     const [money, setMoney] = useState(0)
     const [upgrade1Price, setUpgrade1Price] = useState(50)
@@ -23,45 +25,45 @@ export function Game() {
     const upgrade4 = 2
     const upgrade5 = 5
     const upgrade6 = 10
-    const upgradeAritimetricFormule = 0.01
+    const upgradeAritimetricFormule = 0.05
 
 
-    const goal =5000000
+    const goal = 10000000
     
     
 
     const handle1Upgrade =() => {
-        setUpgradeClickAdd (upgradeClickAdd + upgrade1)
+        setUpgradeClickAdd (upgradeClickAdd + (upgrade1 * rebirth))
         setMoney(money - upgrade1Price)
         setUpgrade1Price(upgrade1Price + (upgrade1Price * upgradeAritimetricFormule))
     }
 
     const handle2Upgrade =() => {
-        setClickPerSecondAmount(clickPerSecondAmount + upgrade2)
+        setClickPerSecondAmount(clickPerSecondAmount + (upgrade2 * rebirth))
         setMoney(money - upgrade2Price)
         setUpgrade2Price(upgrade2Price + (upgrade2Price * upgradeAritimetricFormule))
     }
 
     const handle3Upgrade =() => {
-        setClickPerSecondAmount(clickPerSecondAmount + upgrade3)
+        setClickPerSecondAmount(clickPerSecondAmount + (upgrade3 * rebirth))
         setMoney(money - upgrade3Price)
         setUpgrade3Price(upgrade3Price + (upgrade3Price * upgradeAritimetricFormule))
     }
 
     const handle4Upgrade =() => {
-        setClickPerSecondAmount(clickPerSecondAmount + upgrade4)
+        setClickPerSecondAmount(clickPerSecondAmount + (upgrade4 * rebirth))
         setMoney(money - upgrade4Price)
         setUpgrade4Price(upgrade4Price + (upgrade4Price * upgradeAritimetricFormule))
     }
 
     const handle5Upgrade =() => {
-        setClickPerSecondAmount(clickPerSecondAmount + upgrade5)
+        setClickPerSecondAmount(clickPerSecondAmount + (upgrade5 * rebirth))
         setMoney(money - upgrade5Price)
         setUpgrade5Price(upgrade5Price + (upgrade5Price * upgradeAritimetricFormule))
     }
 
     const handle6Upgrade =() => {
-        setClickPerSecondAmount(clickPerSecondAmount + upgrade6)
+        setClickPerSecondAmount(clickPerSecondAmount + (upgrade6 * rebirth))
         setMoney(money - upgrade6Price)
         setUpgrade6Price(upgrade6Price + (upgrade6Price * upgradeAritimetricFormule))
     }
@@ -74,6 +76,11 @@ export function Game() {
         setMoney(money + upgradeClickAdd)
     }
 
+    const handleRebirth = () => {
+        setRebirth(rebirth + rebirth)
+        setRebirthPrice(rebirthPrice * 2.375)
+    }
+
     useEffect (() => {
         const interval = setInterval(() => {
             clickPerSecond();
@@ -82,6 +89,19 @@ export function Game() {
         return () => clearInterval(interval);
     }, [clickPerSecondAmount])
     
+    useEffect (() => {
+        setMoney(0)
+        setUpgrade1Price(50)
+        setUpgrade2Price(200)
+        setUpgrade3Price(1000)
+        setUpgrade4Price(3000)
+        setUpgrade5Price(10000)
+        setUpgrade6Price(15000)
+        setUpgradeClickAdd(1)
+        setClickPerSecondAmount(0)
+    }, [rebirth])
+
+
     const progressWidth = (money / goal) * 100 + '%';
 
     return (
@@ -106,10 +126,10 @@ export function Game() {
             <table className={styles.upgradeArea}>
                 <tr>
                     <td>
-                        <button disabled={money < upgrade1Price | upgradeClickAdd == 200} onClick={handle1Upgrade} type='button'>Upgrade</button>
+                        <button disabled={money < upgrade1Price} onClick={handle1Upgrade} type='button'>Upgrade</button>
                     </td>
                     <td>
-                        <p>Click +{upgrade1} MaxUpgrade=200</p>
+                        <p>Click +{upgrade1}</p>
                         <p>R${parseFloat(upgrade1Price).toFixed(2)}</p>
                     </td>
                 </tr>
@@ -156,6 +176,15 @@ export function Game() {
                     <td>
                         <p>Click per second +{upgrade6}</p>
                         <p>R${parseFloat(upgrade6Price).toFixed(2)}</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                    <button disabled={money < rebirthPrice} onClick={handleRebirth} type='button'>Rebirth</button>
+                    </td>
+                    <td>
+                        <p>Reset all, but gain 2x</p>
+                        <p>R${parseFloat(rebirthPrice).toFixed(2)}</p>
                     </td>
                 </tr>
 
